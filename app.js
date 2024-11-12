f34ba970cee791aed105a5da48fbb8a5e7132ecc
Pravesh <kumarparvesh805.pk@gmail.com>
2024-11-12 12:50:47 +0530

    add navbar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const mongo_url = "mongodb://127.0.0.1:27017/tripLust";
 
@@ -24,6 +25,8 @@ app.set("views", path.join(__dirname,"views"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res) => {
     res.send("Hello, I am root...");
